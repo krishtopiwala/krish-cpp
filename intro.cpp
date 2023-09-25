5676184f10df94ef30a92a78f24adc4bcf6f0a44
Dhaval Solanki <krishtopiwala1724@gmail.com>
2023-09-25 16:45:12 +0530

    first commit

---
+++ intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My name is Krish Topiwala" << endl;
+    cout << "I am student of IT stream" << endl;
+    cout << "Currently i am learning C++ language" << endl;
+    cout << "See my github repositories" << endl;
+    cout << "Want to become a Full Stack Developer";
+    return 0;
+}
