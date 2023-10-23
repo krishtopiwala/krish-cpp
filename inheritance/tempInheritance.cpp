caed20842c0d1dac11f2bf26460e5b4fe89cf30e
unknown <krishtopiwala1724@gmail.com>
2023-10-23 19:44:48 +0530

    fixed inheritance

--- inheritance/tempInheritance.cpp
+++ inheritance/tempInheritance.cpp
@@ -10,6 +10,14 @@ class Base {
 
     public:
     int c = 30;
+    int num1, num2;
+
+    void setNum() {
+        cout << "enter num1: ";
+        cin >> num1;
+        cout << "enter num2: ";
+        cin >> num2;
+    }
 };
 
 class Derived : public Base {
@@ -21,10 +29,19 @@ class Derived : public Base {
         cout << "The value of b is " << b << endl;
         cout << "The value of c is " << c;
     }
+    void calcAdd() {
+        cout << "Addition of two numbers are " << num1 + num2 << endl;
+    }
+    void calcSub() {
+        cout << "Subtraction of two numbers are " << num1 - num2 << endl;
+    }
 };
 
 int main() {
     Derived d;
+    d.setNum();
     d.getter();
+    d.calcAdd();
+    d.calcSub();
     return 0;
 }
